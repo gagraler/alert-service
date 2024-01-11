8c63541221a1eab5e875beb797d402362ea22f9c
许怀安 <keington@outlook.com>
2024-01-12 00:35:40 +0800

    add: receive alert struct data scripts.

---
+++ scripts/receive_alert.py
@@ -0,0 +1,32 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+# author:  x.gallagher.anderson@gmail.com
+# time: 2024/1/11 21:45
+# description: This script is used to receive alert struct from AlertManager
+
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route("/api/receive", methods=["POST"])
+def receive_handler():
+    
+    """ 
+     Receive alert struct from AlertManager
+     @return: Alert data struct
+     """
+    
+    data = request.get_json()
+    print("Alert data struct:\n", data)
+
+    resp_data = {
+        "token": "3d7f1f8e5a2c4468b9f4661290c6c615",
+    }
+
+    print(json.dumps(resp_data, indent=4, sort_keys=True))
+
+    return jsonify(resp_data)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=18080)
